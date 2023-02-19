03538509c844fcb1562d52cabd7653419a1bbcde
sandza2905 <125198711+sandza2905@users.noreply.github.com>
2023-02-19 22:04:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# python3
-
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
@@ -11,34 +9,27 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    mismatches = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i + 1))
-            
+            # Process opening bracket
+            opening_brackets_stack.append(Bracket(next, i+1))
 
-      elif next in ")]}":
- if len(opening_brackets_stack) == 0:
-                mismatches.append(Bracket(next, i + 1))
-            else:
-                top = opening_brackets_stack.pop()
-                if not are_matching(top.char, next):
-                    mismatches.append(Bracket(next, i + 1))
-    if len(mismatches) > 0:
-        return mismatches[0].position
-    elif len(opening_brackets_stack) > 0:
+        if next in ")]}":
+            # Process closing bracket
+            if len(opening_brackets_stack) == 0:
+                return i+1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
+                return i+1
+    if len(opening_brackets_stack) > 0:
         return opening_brackets_stack[0].position
-    else:
-        return "Success"
+    return "Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    if mismatch == "Success":
-        print(mismatch)
-    else:
-        print(mismatch)
+    print(mismatch)
 
 
 if __name__ == "__main__":
