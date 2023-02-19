e5aa710f362494fe98553d5171129e200b7d387d
sandza2905 <125198711+sandza2905@users.noreply.github.com>
2023-02-19 23:46:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,24 @@ def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
 def find_mismatch(text):
-opening_brackets_stack = []
-for i, next_char in enumerate(text):
-    
-if next_char in "([{":
-opening_brackets_stack.append(Bracket(next_char, i+1))
+    opening_brackets_stack = []
+    for i, next_char in enumerate(text):
 
-elif next_char in ")]}":
-if not opening_brackets_stack:
-return i+1
+        if next_char in "([{":
+            opening_brackets_stack.append(Bracket(next_char, i+1))
 
-top = opening_brackets_stack.pop()
-if not are_matching(top.char, next_char):
-return i+1
+        elif next_char in ")]}":
+            if not opening_brackets_stack:
+                return i+1
 
-if opening_brackets_stack:
-return opening_brackets_stack[0].position
-return "Success"
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next_char):
+                return i+1
+
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+
+    return "Success"
 
 def main():
 user_input = input("Enter the code: ")
