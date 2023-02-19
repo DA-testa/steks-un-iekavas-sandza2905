fb48893c1891e06687eb22a55ce35b544aaaacbf
sandza2905 <125198711+sandza2905@users.noreply.github.com>
2023-02-19 21:54:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,25 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    mismatches = []
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i + 1))
             
 
         if next in ")]}":
-            if len(opening_brackets_stack) == 0:
-                return i+1
-            top = opening_brackets_stack.pop()
-            if not are_matching(top.char, next):
-                return i+1
-    if len(opening_brackets_stack) > 0:
+ if len(opening_brackets_stack) == 0:
+                mismatches.append(Bracket(next, i + 1))
+            else:
+                top = opening_brackets_stack.pop()
+                if not are_matching(top.char, next):
+                    mismatches.append(Bracket(next, i + 1))
+    if len(mismatches) > 0:
+        return mismatches[0].position
+    elif len(opening_brackets_stack) > 0:
         return opening_brackets_stack[0].position
-    return "Success"
+    else:
+        return "Success"
 
 
 def main():
